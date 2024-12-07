4ed8419474e892506d2ba5dc6a34eefd82abe009
yraj2001 <yaswamthrajvarikunta@gmail.com>
2024-12-07 02:13:07 -0600

    Build game screenshots

--- src/components/GameTrailer.tsx
+++ src/components/GameTrailer.tsx
@@ -1,4 +1,3 @@
-import { Spinner } from "@chakra-ui/react";
 import useTrailers from "../hooks/useTrailers";
 
 interface Props {
@@ -8,7 +7,7 @@ interface Props {
 const GameTrailer = ({ gameId }: Props) => {
   const { data, error, isLoading } = useTrailers(gameId);
 
-  if (isLoading) return <Spinner />;
+  if (isLoading) return null;
 
   if (error) throw error;
 
